2fab146b3307331317bd5295ac7eedd875154043
oleh <future18straat@gmail.com>
2024-07-02 20:27:47 +0200

    закінчив з дз

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+    let contentWidth = parseFloat(content.slice(0, -2));
+    let paddingWidth = parseFloat(padding.slice(0, -2)) * 2;
+    let borderWidth = parseFloat(border.slice(0, -2)) * 2;
+    const totalWidth = contentWidth + paddingWidth + borderWidth;
+    return totalWidth;
+}
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
